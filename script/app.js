69ea7e11b2b43494aa1d246b9a0806826c00b239
Anamul Hassan <mdanamolhasan1998@gmail.com>
2025-03-14 16:58:54 +0600

    banner desing complited

--- script/app.js
+++
@@ -1,6 +0,0 @@
-const loadCategory = async ()=> {
-    const response = await fetch('https://openapi.programming-hero.com/api/peddy/categories')
-    const data = await response.json()
-    console.log(data.categories) 
-}
-loadCategory()
